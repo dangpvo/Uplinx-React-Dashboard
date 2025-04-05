7e91476582444c26e70c7be84f98e14edb6ecd2b
Dennis Vo <dennisphuc@gmail.com>
2025-04-05 13:55:58 +0200

    feat: finish StatCard in Overview page with animation

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import Header from "../components/common/Header";
+import { motion } from "framer-motion";
+import StatCard from "../components/common/StatCard";
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
 
 const Overview = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title={"Overview"}></Header>
+
+      <main className="max-w-7xl mx-auto py-8 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard
+            name="Total Sales"
+            icon={Zap}
+            value="$12.34"
+            color="#6366f1"
+          ></StatCard>
+          <StatCard
+            name="New Users"
+            icon={Users}
+            value="1234"
+            color="#8b5cf6"
+          ></StatCard>
+          <StatCard
+            name="Total Products"
+            icon={ShoppingBag}
+            value="245"
+            color="#ec4899"
+          ></StatCard>
+          <StatCard
+            name="Conversion Rate"
+            icon={BarChart2}
+            value="12.34%"
+            color="#10b981"
+          ></StatCard>
+        </motion.div>
+      </main>
     </div>
   );
 };
