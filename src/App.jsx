0953ed683bfd0a6216bbfea3de4cccf1f5d8a749
Dennis Vo <dennisphuc@gmail.com>
2025-04-05 13:35:32 +0200

    feat: finish Sidebar setup with routes and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import Sidebar from "./components/Sidebar";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"></div>
+        <div className="absolute inset-0 backdrop-blur-sm"></div>
+      </div>
 
-  return <div className="text-red-500 rounded "> hahaha</div>;
-}
+      <Sidebar></Sidebar>
+      <Routes>
+        <Route path="/" element={<OverviewPage />}></Route>
+        <Route path="/products" element={<ProductsPage />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
